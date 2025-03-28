7423bb22e06e5dc05d413bd2f37e7df79693205e
Mark Muthii <mark.muthii@strathmore.edu>
2025-03-28 10:46:16 +0300

    Update Input to have extra children

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,7 +1,7 @@
 const Input = (props) => {
   return (
     <input
-      className="border border-gray-300 p-2 rounded"
+      className={`${props.className} border border-gray-300 p-2 rounded`}
       {...props} // Spread operator to pass all props to the input element
     />
   );
